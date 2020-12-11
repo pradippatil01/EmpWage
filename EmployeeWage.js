6a95802366971d17b8ded974869363ed33c55318
pradippatil01 <mr.pradip01@gmail.com>
2020-12-11 15:14:27 +0530

    Add : added calDailyWage function and calculate wage if full timepresent

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,10 +1,25 @@
 console.log("Welcone to employee wage computation");
+let empCheck;
+let empWorkPeriod;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
 
 /* check employee status */
-let empCheck = Math.floor(Math.random() * 10) % 2;
+empCheck = Math.floor(Math.random() * 10) % 2;
 if (empCheck == 1) {
     console.log("employee is present");
+    calDailyWage();
 }
 if (empCheck == 0) {
     console.log("employee is absent");
+}
+
+function calDailyWage()
+{
+   empWorkPeriod = Math.floor(Math.random() * 10) % 2;
+   if(empWorkPeriod==1)
+   {
+   let wage=FULL_TIME_HOURS*WAGE_PER_HOUR;
+   console.log("employee full time present and wage : "+wage);
+   }
 }
