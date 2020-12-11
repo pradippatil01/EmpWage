465ab0fd99f8e1a9a77e63e2e8722496e247d627
pradippatil01 <mr.pradip01@gmail.com>
2020-12-11 17:27:04 +0530

    Add : Added array to store daily wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,6 +1,6 @@
 console.log("Welcone to employee wage computation");
 let empCheck;
-let totalWage;
+var totalWage = 0;
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const IS_ABSENT = 0;
@@ -9,8 +9,9 @@ const PART_TIME_HOURS = 4;
 const WAGE_PER_HOUR = 20;
 const NUM_OF_WORKING_DAYS = 20;
 const MAX_HOURS_IN_MONTH = 100;
-let workingDays = 1;
-let totalWorkingHours = 0;
+var totalWorkingDays = 0;
+var totalWorkingHours = 0;
+var empDailyWageArr = new Array();
 function getWorkingHours() {
     empCheck = Math.floor(Math.random() * 10) % 3;
     switch (empCheck) {
@@ -25,14 +26,29 @@ function getWorkingHours() {
             break;
     }
 }
+
 function main() {
-    while (workingDays <= NUM_OF_WORKING_DAYS && totalWorkingHours <= MAX_HOURS_IN_MONTH) {
-        workingDays++;
+    while (totalWorkingDays <= NUM_OF_WORKING_DAYS && totalWorkingHours <= MAX_HOURS_IN_MONTH) {
+        totalWorkingDays++;
         let empHrs = getWorkingHours();
         totalWorkingHours += empHrs;
+        empDailyWageArr.push(
+            {
+                dailyWage: calDailyWage(empHrs),
+                toString() {
+                    return "\n Daily Wage Earned => " + this.dailyWage
+                }
+            });
     }
-    totalWage = totalWorkingHours * WAGE_PER_HOUR;
     console.log("Total wage of month is : " + totalWage);
+    console.log(empDailyWageArr.toString());
+}
+
+/* calculate daily wage */
+function calDailyWage(empHrs) {
+    let dailyWage = empHrs * WAGE_PER_HOUR;
+    totalWage += dailyWage;
+    return dailyWage;
 }
 
 main();
