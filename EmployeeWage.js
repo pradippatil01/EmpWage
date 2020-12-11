3c855af81a33f4b1992c828eee4f273f8554fd17
pradippatil01 <mr.pradip01@gmail.com>
2020-12-11 15:01:17 +0530

    Add : check employee status using random function

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,1 +1,10 @@
 console.log("Welcone to employee wage computation");
+
+/* check employee status */
+let empCheck = Math.floor(Math.random() * 10) % 2;
+if (empCheck == 1) {
+    console.log("employee is present");
+}
+if (empCheck == 0) {
+    console.log("employee is absent");
+}
