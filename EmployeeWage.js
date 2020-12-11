6f78a7cc587212fadca61158e648b9e218f40f9f
pradippatil01 <mr.pradip01@gmail.com>
2020-12-11 15:29:52 +0530

    Add : calculated wage for parttime and code refractored created function

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,25 +1,34 @@
 console.log("Welcone to employee wage computation");
 let empCheck;
 let empWorkPeriod;
+let totalWage;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
 const FULL_TIME_HOURS = 8;
+const PART_TIME_HOURS = 4;
 const WAGE_PER_HOUR = 20;
+empCheck = Math.floor(Math.random() * 10) % 2;
+checkEmployeeStatus(empCheck);
 
 /* check employee status */
-empCheck = Math.floor(Math.random() * 10) % 2;
-if (empCheck == 1) {
-    console.log("employee is present");
-    calDailyWage();
-}
-if (empCheck == 0) {
-    console.log("employee is absent");
+function checkEmployeeStatus(empCheck) {
+    if (empCheck == 1) {
+        calDailyWage();
+    }
+    else {
+        console.log("employee is Absent");
+    }
 }
 
-function calDailyWage()
-{
-   empWorkPeriod = Math.floor(Math.random() * 10) % 2;
-   if(empWorkPeriod==1)
-   {
-   let wage=FULL_TIME_HOURS*WAGE_PER_HOUR;
-   console.log("employee full time present and wage : "+wage);
-   }
+function calDailyWage() {
+    empWorkPeriod = Math.floor(Math.random() * 10) % 2;
+    if (empWorkPeriod == IS_FULL_TIME) {
+        totalWage = FULL_TIME_HOURS * WAGE_PER_HOUR;
+        console.log("employee full time present and wage : " + totalWage);
+    }
+    if (empWorkPeriod == IS_PART_TIME) {
+        totalWage = PART_TIME_HOURS * WAGE_PER_HOUR;
+        console.log("employee part time present and wage : " + totalWage);
+    }
 }
+
